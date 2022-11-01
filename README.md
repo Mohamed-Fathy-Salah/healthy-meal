196b605940bffdd4b6a8b921fb148fc662345890
Mohamed Fathy <mofasa.peace@gmail.com>
2022-11-01 15:14:08 +0200

    oauth with facebook, google

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 backend for healthy meal app
 
 # How to Run
-`docker run mofasa101/healthy-meal`
+`docker run -p4000:4000 mofasa101/healthy-meal`
+
+open `http://localhost:4000/graphql` in your browser
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,11 @@ import FollowResolver from "./resolvers/follow-resolver";
 import MealResolver from "./resolvers/meal-resolver";
 import LikeResolver from "./resolvers/like-resolver";
 import BookmarkResolver from "./resolvers/bookmark-resolver";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth2";
+import { Strategy as FacebookStrategy } from "passport-facebook";
+import jwt from "jsonwebtoken";
+import User from "./entity/user";
 
 export const createApolloServer = async (port: number | string) => {
   const app = express();
@@ -15,6 +20,8 @@ export const createApolloServer = async (port: number | string) => {
   app.set("trust proxy", 1);
 
   app.use(cookieSession({ signed: false }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
@@ -32,6 +39,112 @@ export const createApolloServer = async (port: number | string) => {
   });
 
   apolloServer.applyMiddleware({ app, cors: false });
+
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  const findOrCreate = async (profile: any) => {
+    const user = await User.findOne({
+      where: { email: profile.emails[0].value },
+      select: ["user_id", "name", "email", "photo", "gender"],
+    });
+
+    console.log(user);
+    if (!user) {
+      const userData = {
+        email: profile.emails[0].value,
+        name: profile.displayName,
+        password: profile.id,
+        photo: profile.photos[0].value,
+        gender: profile.gender,
+      };
+
+      if (!userData.photo) delete userData.photo;
+      if (!userData.gender) delete userData.gender;
+
+      const { identifiers } = await User.insert(userData);
+
+      delete userData.password;
+      return { ...userData, user_id: identifiers[0].user_id };
+    }
+
+    return {
+      user_id: user.user_id,
+      email: user.email,
+      name: user.name,
+      photo: user.photo,
+      gender: user.gender,
+    };
+  };
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID:
+          "566127781877-1gpm82mb9at3lhchr6gr5d7ebcsh06cc.apps.googleusercontent.com",
+        clientSecret: "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",
+        callbackURL: `http://localhost:${port}/auth/google/callback`,
+      },
+      async (_, __, profile, cb) => {
+        cb(null, await findOrCreate(profile));
+      }
+    )
+  );
+
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      session: false,
+      scope: ["email", "profile"],
+    })
+  );
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { session: false }),
+    async (req, res) => {
+      const user = req.user as User;
+      const userJWT = jwt.sign({ id: user.user_id, email: user.email }, "asdf");
+      req.session = {
+        jwt: userJWT,
+      };
+      res.send({ user: req.user });
+    }
+  );
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: "883510893026660",
+        clientSecret: "7498fb055d6b64615f579bf9774e5f8f",
+        callbackURL: `http://localhost:${port}/auth/facebook/callback`,
+        profileFields: ["id", "displayName", "photos", "email", "gender"],
+      },
+      async (_, __, profile, cb) => {
+        cb(null, await findOrCreate(profile));
+      }
+    )
+  );
+
+  app.get(
+    "/auth/facebook",
+    passport.authenticate("facebook", {
+      session: false,
+    })
+  );
+
+  app.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook", { session: false }),
+    async (req, res) => {
+      const user = req.user as User;
+      const userJWT = jwt.sign({ id: user.user_id, email: user.email }, "asdf");
+      req.session = {
+        jwt: userJWT,
+      };
+      res.send({ user: req.user });
+    }
+  );
 
   const httpServer = app.listen(port, () => {
     console.log(`server started at http://localhost:${port}/graphql`);
